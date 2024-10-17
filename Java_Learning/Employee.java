c4b0638460e5c5645713d3b183456a09b822d180
Zhixing Zhang <1715017258@qq.com>
2024-10-18 00:58:44 +0100

    10.18\

---
+++ Java_Learning/Employee.java
@@ -0,0 +1,59 @@
+public class Employee {
+    // Attributes
+    private int employeeNumber;
+    private String familyName;
+    private String givenName;
+    private double basicHourlyRate;
+    private int contractedHoursPerMonth;
+    private int hoursWorked;
+    
+    public static void main(String[] args) {
+        // HelloWorld h=new HelloWorld();
+        // int [] ver=new int[10];
+        // h.swi("Hello, World");
+
+        String[] p={"hello","world"};
+        for(String a:p){
+            System.out.println(a);
+        }
+    }
+    // Constructor
+    public Employee(int employeeNumber, String familyName, String givenName,
+                    double basicHourlyRate, int contractedHoursPerMonth) {
+        this.employeeNumber = employeeNumber;
+        this.familyName = familyName;
+        this.givenName = givenName;
+        this.basicHourlyRate = basicHourlyRate;
+        this.contractedHoursPerMonth = contractedHoursPerMonth;
+        this.hoursWorked = 0; // Initial value for hours worked is zero
+    }
+
+    // Getters and Setters (if needed)
+    public int getEmployeeNumber() {
+        return employeeNumber;
+    }
+
+    public String getFamilyName() {
+        return familyName;
+    }
+
+    public String getGivenName() {
+        return givenName;
+    }
+
+    public double getBasicHourlyRate() {
+        return basicHourlyRate;
+    }
+
+    public int getContractedHoursPerMonth() {
+        return contractedHoursPerMonth;
+    }
+
+    public int getHoursWorked() {
+        return hoursWorked;
+    }
+
+    public void setHoursWorked(int hoursWorked) {
+        this.hoursWorked = hoursWorked;
+    }
+}
---
+++ Java_Learning/HelloWorld.java
@@ -0,0 +1,37 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        // int a=5,b=10;
+        String s="Hello, World!";
+        swi(s);
+        // System.out.println(add(a,b)+s);
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public void swi(String s) {
+        int vs = 0, cs = 0;
+        for (int i=0; i<s.length(); i++) {
+        char c = s.charAt(i);
+        switch (c) {
+        case 'a':
+        break;
+        case 'e':
+        // …
+        vs++;
+        break;
+        case 'b':
+        case 'c':
+        // …
+        cs++;
+        break;
+        default:
+        System.out.println ("other");
+        }
+        }
+        System.out.println("Vowels:"+ vs + "Consonants:"+ cs);
+    }
+}
